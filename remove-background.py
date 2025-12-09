75376be412f196847898d2f943e74a445e10ac5b
Ajman <mocsanaajman@gmail.com>
2025-12-09 23:45:10 +0800

    refactor: rename the variable names

--- remove-background.py
+++ remove-background.py
@@ -5,11 +5,11 @@ from PIL import Image
 model_name = "u2net_human_seg"
 session = new_session(model_name) 
 
-input_path = 'input_image.png' 
-output_path = 'output_new_model.png'
+input_image_path = 'input_image.png' 
+output_image_path = 'output_new_model.png'
 
-input_image = Image.open(input_path)
+input_image = Image.open(input_image_path)
 output_image = remove(input_image, session=session)
 
-output_image.save(output_path)
-print(f"Background removed using {model_name}. Saved to: {output_path}")
+output_image.save(output_image_path)
+print(f"Background removed using {model_name}. Saved to: {output_image_path}")
